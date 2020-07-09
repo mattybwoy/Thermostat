9dbe2c28bda3eaad8994690f252b78519b16173e
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-09 19:55:57 +0100

    Added HTML and CSS files, providing interface for thermostat

--- src/thermostat.js
+++ src/thermostat.js
@@ -10,8 +10,7 @@ class Thermostat {
 
   up() {
     if (this.powerSaver == false && this.maximumTemp <= this.temperature) {throw new Error("Maximum temperature exceeded!!");}
-    // if (this.powerSaverOff() && this.maximumTemp < this.temperature) {throw new Error("Maximum temperature exceeded!!");}
-    if (this.powerSaver == true && this.maximumTemp <= this.temperature) {throw new Error("Maximum temperature exceeded!");}
+    else if (this.powerSaver == true && this.maximumTemp <= this.temperature) {throw new Error("Maximum temperature exceeded!");}
     this.temperature++;
     return this.temperature;
   };
